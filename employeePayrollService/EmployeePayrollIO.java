5eb95c181e7437a228ae15f2ec3e33fa854484de
Himanshu8922 <himanshurai9211@gmail.com>
2022-10-20 23:35:04 +0530

    to read and write employee payroll to console

---
+++ employeePayrollService/EmployeePayrollIO.java
@@ -0,0 +1,22 @@
+package employeePayrollService;
+
+//EmployeePayrollIO Class
+public class EmployeePayrollIO {
+	public int ID;
+	public String Name;
+	public double Salary;
+
+	// EmployeePayrollIO Constructor
+	public EmployeePayrollIO(int ID, String Name, double Salary) {
+		this.ID = ID;
+		this.Name = Name;
+		this.Salary = Salary;
+	}
+
+	// EmployeePayrollIO Override function to display
+	@Override
+	public String toString() {
+		return "ID ->" + ID + ",NAME ->" + Name + ",SALARY ->" + Salary;
+	}
+
+}
